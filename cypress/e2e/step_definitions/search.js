bfb7570adeeab533e0b54ab3929456cf3664dbf8
UJJWAL MANGAL ATREYA <ujjwalmangalatreya@UJJWALs-MacBook-Pro.local>
2023-09-19 10:43:27 +0545

    Dynamic endpoint interception

--- cypress/e2e/step_definitions/search.js
+++ cypress/e2e/step_definitions/search.js
@@ -2,10 +2,12 @@ import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 import { searchPage } from "@pages/SearchPage";
 import { resultPage } from "@pages/ResultsPage";
 
+
 Given("A browser with property finder page", () => {
   cy.visit("/");
-  cy.intercept('GET', 'https://www.propertyfinder.bh/_next/data/28cLQ2qzHoVye0kbkifJi/en/search.json?c=1&t=35&pt=300000&fu=0&ob=mr').as('viallaResult')
-  cy.intercept('GET','https://www.propertyfinder.bh/_next/data/28cLQ2qzHoVye0kbkifJi/en/search.json?c=3&t=4&fu=0&ob=mr').as('commercialOfficeResults')
+  cy.intercept('GET', '**/_next/data/*/en/search.json?c=1&t=35&pt=300000&fu=0&ob=mr').as('viallaResult')
+  cy.intercept('GET','**/_next/data/*/en/search.json?c=3&t=4&fu=0&ob=mr').as('commercialOfficeResults')
+ 
 });
 
 When('User checks commerial proerties only and selects office categories',()=>{
